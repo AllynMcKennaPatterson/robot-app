6785bd0bcf780ef2bb6b6745e8e39c3c696f3667
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-12-14 19:56:32 +0000

    Created header component

--- components/header/header.js
+++ components/header/header.js
@@ -1,9 +1,27 @@
+import classes from "./header.module.css";
+import NavButton from "../nav-buttons/nav-buttons";
+import Link from "next/link";
 
-function Header(){
-    return(
-        <div>
+function Header() {
+  return (
+    <div className={classes.headerContainer}>
+      <div className={classes.navContainer}>
+        <Link href={"./"} className={classes.buttonLink}>
+          <NavButton text={"Home"} />
+        </Link>
+            <img src="assets/images/ModuGrip-Logo.svg" alt="ModuGrip Logo" className={classes.logo}/>
+        <div className={classes.authButtons}>
+          <Link href={"./login"} className={classes.buttonLink}>
+            <NavButton text={"Log In"} className={classes.button}/>
+          </Link>
+          <div className={classes.divider}/>
+          <Link href={"/signup"} className={classes.buttonLink}>
+            <NavButton text={"Sign Up"} />
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Header;
