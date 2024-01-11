16e9ed963c62af829e0bca9e5c880c2fb9807e93
Allyn McKenna Patterson <allyncodes@gmail.com>
2024-01-11 19:23:49 +0000

    Added ability to open slider modal

--- components/control-buttons/slider-control-button/slider-control-button.js
+++ components/control-buttons/slider-control-button/slider-control-button.js
@@ -1,8 +1,11 @@
 import classes from "./slider-control-button.module.css";
 
-function SliderControlButton() {
+function SliderControlButton(props) {
   return (
-    <div className={classes.controlContainer}>
+    <div
+      className={classes.controlContainer}
+      onClick={() => props.setOpenSliders(true)}
+    >
       <div className={classes.buttonContent}>
         <img src="/assets/images/control-buttons/slider-icon.svg" />
       </div>
