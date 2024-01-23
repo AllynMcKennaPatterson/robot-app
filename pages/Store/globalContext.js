003e2d27f64985d1fa95a97edea610e939ea4ad2
Allyn McKenna Patterson <allyncodes@gmail.com>
2024-01-23 19:11:46 +0000

    Fixed global context

--- pages/Store/globalContext.js
+++ pages/Store/globalContext.js
@@ -1,33 +1,36 @@
-// import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 
-// const GlobalContext = createContext();
+const GlobalContext = createContext({
+  // loggedIn: false
+});
 
-// export function GlobalContextProvider() {
-//     const [globals, setGlobals] = useState(defaultGlobals);
-//   async function publishAction(action) {
-//     console.log("We got this far: " + action);
-//     // const response = await fetch("/api/publish-slider", {
-//     //   method: "POST",
-//     //   body: JSON.stringify(action),
-//     //   headers: {
-//     //     "Content-Type": "application/json",
-//     //     "X-Auth-Token": "BBUS-R9TZ8sPj4ctPgpHE6HEOCIsvAwixSx",
-//     //   },
-//     // });
-//     // const data = await response.json();
-//     // console.log(data);
-//   }
+export function GlobalContextProvider(props) {
+  // const [globals, setGlobals] = useState(defaultGlobals);
 
-//   const context = {
-//     publishAction: publishAction,
-//     // theGlobalObject: globals,
-//   };
+  async function publishActionHandler(action) {
+    console.log("We got this far: " + JSON.stringify(action));
+    const response = await fetch("/api/publish-slider", {
+      method: "POST",
+      body: JSON.stringify(action),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("We got here 2")
+    const data = await response.json();
+    console.log(data);
+  }
 
-//   return (
-//     <GlobalContext.Provider value={context}>
-//       {/* {props.children} */}
-//     </GlobalContext.Provider>
-//   );
-// }
+  const context = {
+    publishAction: publishActionHandler,
+    // theGlobalObject: globals,
+  };
 
-// export default GlobalContext;
+  return (
+    <GlobalContext.Provider value={context}>
+      {props.children}
+    </GlobalContext.Provider>
+  )
+}
+
+export default GlobalContext;
