725ce597ccefbd6773eaa8fd763f933d5ad5f3e9
G00392765@atu.ie <G00392765@atu.ie>
2024-01-25 09:47:48 +0000

    Open slider modal will now close coordinate modal

--- components/control-buttons/end-effector-control-button/end-effector-control-button.js
+++ components/control-buttons/end-effector-control-button/end-effector-control-button.js
@@ -1,13 +1,18 @@
 import classes from "./end-effector-control-button.module.css";
 
-function EndEffectorControlButton() {
+function EndEffectorControlButton(props) {
+  function clickEvent(){
+    props.setOpenSliders(false)
+    props.setOpenCoordinateModal(true)
+  }
   return (
 
-    <div className={classes.controlContainer}>
+    <div className={classes.controlContainer}
+      onClick={() => clickEvent()}>
       <div className={classes.buttonContent}>
         <img src="/assets/images/control-buttons/end-effector-icon.svg" />
       </div>
-        
+
     </div>
   );
 }
