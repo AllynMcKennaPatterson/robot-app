f9b3207ed2953c88daad07e96336f741be17f89a
G00392765@atu.ie <G00392765@atu.ie>
2024-01-29 10:49:05 +0000

    Created static base mesh

--- components/react-three-fiber/robot-model-simple/robot-base/robot-base.js
+++ components/react-three-fiber/robot-model-simple/robot-base/robot-base.js
@@ -2,24 +2,20 @@ import ModelContext from "@/pages/Store/modelContext";
 import { useFrame } from "@react-three/fiber";
 import { useContext, useEffect, useRef, useState } from "react";
 
-function RobotBaseSimple(props) {
+function RobotBase(props) {
   const ref = useRef();
   const modelState = useContext(ModelContext)
 
   useEffect(() => {
-    modelState.updateModelState({ joint: "joint1", angle: 10 });
-    console.log("ModelState: " + modelState.theModelData.baseSimpleRotation)
+    modelState.updateModelState({ joint: "joint1", angle: 90 });
   }, [])
 
   return (
-    <group position={[0, 0, 0]} rotation={modelState.theModelData.baseSimpleRotation}>
       <mesh position={props.position} ref={ref} >
         <boxGeometry args={props.size} />
         <meshStandardMaterial color={props.color} />
       </mesh>
-    </group>
-
   );
 }
 
-export default RobotBaseSimple;
+export default RobotBase;
