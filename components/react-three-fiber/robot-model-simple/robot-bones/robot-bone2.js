1a7dd0741996c33543953e5dcb100e06e5eb5b67
G00392765@atu.ie <G00392765@atu.ie>
2024-01-29 12:09:35 +0000

    Created robot bone 2

---
+++ components/react-three-fiber/robot-model-simple/robot-bones/robot-bone2.js
@@ -0,0 +1,24 @@
+import ModelContext from "@/pages/Store/modelContext";
+import { useFrame } from "@react-three/fiber";
+import { useContext, useEffect, useRef, useState } from "react";
+
+function RobotBone2(props) {
+  const ref = useRef();
+  const modelState = useContext(ModelContext)
+
+  useEffect(() => {
+    modelState.updateModelState({ joint: "joint2", angle: 0 });
+  }, [])
+
+  return (
+    <group position={[0, 0, 0]} rotation={modelState.theModelData.bone2Rotation}>
+      <mesh position={props.position} ref={ref} >
+        <boxGeometry args={props.size} />
+        <meshStandardMaterial color={props.color} />
+      </mesh>
+    </group>
+
+  );
+}
+
+export default RobotBone2;
