3d5465b0fa292e234d1b2a391dd0da62a9d104cb
G00392765@atu.ie <G00392765@atu.ie>
2024-01-29 10:50:56 +0000

    Edited camera angle Added meshes to group to contain the whole robot Added Base mesh

--- components/react-three-fiber/test-scene/test-scene.js
+++ components/react-three-fiber/test-scene/test-scene.js
@@ -2,7 +2,8 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import classes from "./test-scene.module.css";
 import { useState } from "react";
 import GroundPlane from "../ground-plane/ground-plane";
-import RobotBaseSimple from "../robot-model-simple/robot-base-simple/robot-base-simple";
+import RobotBone1 from "../robot-model-simple/robot-bones/robot-bone1";
+import RobotBase from "../robot-model-simple/robot-base/robot-base";
 // import { OrbitControls } from "@react-three/drei";
 
 function TestScene() {
@@ -11,11 +12,15 @@ function TestScene() {
 
   return (
     <div className={classes.sceneContainer}>
-      <Canvas camera={{ fov: 40, position: [-10, 4, 2]}}>
+      <Canvas camera={{ fov: 40, position: [-14, 8, 3]}}>
         <directionalLight position={[0, 0, 2]} intensity={1} />
         <ambientLight intensity={1} />
         <GroundPlane/>
-          <RobotBaseSimple position={[0, 0, -1]} color={"#0095e6"} size={[2, 1, 2]} rotation={[0, 0, 0]}/> 
+        <group position={[0, 0,-1]}>
+          <RobotBase position={[0, 0.5, 0]} color={"#0095e6"} size={[2, 1, 3]} rotation={[0, 0, 0]}/>
+          <RobotBone1 position={[0, 0, -2]} color={"#0095e6"} size={[1, 1, 4]} rotation={[0, 0, 0]}/> 
+        </group>
+        
         {/* <TestCube position={[0, 0, 0]} color={"#0095e6"} size={[1, 1, 1]} /> */}
         {/* <Torus position={[2, 0, 0]} size={[0.5, 0.1, 30, 30]} color={"hotpink"}/> */}
         {/* <OrbitControls/> */}
