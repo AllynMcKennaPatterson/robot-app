8fe1be27f810b93b6f192a0a284f156c9922bfca
G00392765@atu.ie <G00392765@atu.ie>
2024-01-29 12:09:59 +0000

    Added rotation to group for joint 1

--- components/react-three-fiber/test-scene/test-scene.js
+++ components/react-three-fiber/test-scene/test-scene.js
@@ -1,14 +1,20 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import classes from "./test-scene.module.css";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import GroundPlane from "../ground-plane/ground-plane";
 import RobotBone1 from "../robot-model-simple/robot-bones/robot-bone1";
 import RobotBase from "../robot-model-simple/robot-base/robot-base";
+import RobotBone2 from "../robot-model-simple/robot-bones/robot-bone2";
+import ModelContext from "@/pages/Store/modelContext";
 // import { OrbitControls } from "@react-three/drei";
 
 function TestScene() {
   const [ baseRotation, setBaseRotation] = useState([0,0,0]);
+  const modelState = useContext(ModelContext)
 
+  useEffect(() => {
+    modelState.updateModelState({ joint: "joint1", angle: 90 });
+  }, [])
 
   return (
     <div className={classes.sceneContainer}>
@@ -18,13 +24,14 @@ function TestScene() {
         <GroundPlane/>
         <group position={[0, 0,-1]}>
           <RobotBase position={[0, 0.5, 0]} color={"#0095e6"} size={[2, 1, 3]} rotation={[0, 0, 0]}/>
-          <RobotBone1 position={[0, 0, -2]} color={"#0095e6"} size={[1, 1, 4]} rotation={[0, 0, 0]}/> 
+          <group rotation={modelState.theModelData.bone1Rotation}> {/* Joint 1 */}
+            <RobotBone1 position={[0, 0, -2]} color={"#0095e6"} size={[1, 1, 4]} rotation={[0, 0, 0]}/> 
+            <RobotBone2 position={[0, 0, -2]} color={"#0095e6"} size={[1, 1, 4]} rotation={[0, 0, 0]}/> 
+          </group>
+          
+
         </group>
         
-        {/* <TestCube position={[0, 0, 0]} color={"#0095e6"} size={[1, 1, 1]} /> */}
-        {/* <Torus position={[2, 0, 0]} size={[0.5, 0.1, 30, 30]} color={"hotpink"}/> */}
-        {/* <OrbitControls/> */}
-        <gridHelper />
       </Canvas>
     </div>
   );
