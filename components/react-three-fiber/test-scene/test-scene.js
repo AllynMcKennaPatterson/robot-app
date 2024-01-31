3b4cf922bb80060e61ac730922de18d7e2771b8f
G00392765@atu.ie <G00392765@atu.ie>
2024-01-31 14:19:37 +0000

    Added conditional rendering for target block

--- components/react-three-fiber/test-scene/test-scene.js
+++ components/react-three-fiber/test-scene/test-scene.js
@@ -6,10 +6,11 @@ import RobotBone1 from "../robot-model-simple/robot-bones/robot-bone1";
 import RobotBase from "../robot-model-simple/robot-base/robot-base";
 import RobotBone2 from "../robot-model-simple/robot-bones/robot-bone2";
 import ModelContext from "@/pages/Store/modelContext";
+import TestCube from "../test-cube/test-cube";
 // import { OrbitControls } from "@react-three/drei";
 
 function TestScene() {
-  const [ baseRotation, setBaseRotation] = useState([0,0,0]);
+  const [baseRotation, setBaseRotation] = useState([0, 0, 0]);
   const modelState = useContext(ModelContext)
 
   useEffect(() => {
@@ -18,18 +19,18 @@ function TestScene() {
 
   return (
     <div className={classes.sceneContainer}>
-      <Canvas camera={{ fov: 40, position: [-14, 8, 3]}}>
-        <directionalLight position={[0, 0, 2]} intensity={1} />
+      <Canvas camera={{ fov: 40, position: [-14, 6, 8] }}>
+        <directionalLight position={[-2, 6, -2]} intensity={2} />
         <ambientLight intensity={1} />
-        <GroundPlane/>
-        <group position={[0, 0,-1]}>
-          <RobotBase position={[0, 0.5, 0]} color={"#0095e6"} size={[2, 1, 3]} rotation={[0, 0, 0]}/>
+        <GroundPlane />
+        <group position={[0, 0, 0]} rotation={modelState.theModelData.baseRotation}>
+          <RobotBase position={[0, 0.5, 0]} color={"#0095e6"} size={[2, 1, 3]} rotation={[0, 0, 0]} />
           <group rotation={modelState.theModelData.bone1Rotation}> {/* Joint 1 */}
-            <RobotBone1 position={[0, 0, -2]} color={"#0095e6"} size={[1, 1, 6]} rotation={[0, 0, 0]}/> 
-            <RobotBone2 position={[0, 0, -1]} color={"#0095e6"} size={[1, 1, 2]} rotation={[0, 0, 0]}/> 
-          </group>
+            <RobotBone1 position={[0, 0, -2]} color={"#0095e6"} size={[1, 1, 6]} rotation={[0, 0, 0]} />
+            <RobotBone2 position={[0, 0, -1]} color={"#0095e6"} size={[1, 1, 2]} rotation={[0, 0, 0]} />
+           </group>
         </group>
-        
+        { modelState.targetBlock == true ? <TestCube position={[-3, 1, 3]} size={[1, 1, 1]} color={"#ff711f"}/> : console.log("Hidden block: " + modelState.targetBlock)}
       </Canvas>
     </div>
   );
