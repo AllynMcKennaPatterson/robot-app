c083512aa021fcde8719b0db9830318948827b3a
G00392765@atu.ie <G00392765@atu.ie>
2024-02-08 14:41:08 +0000

    Added transform controls

--- components/react-three-fiber/test-cube/test-cube.js
+++ components/react-three-fiber/test-cube/test-cube.js
@@ -1,38 +1,33 @@
 import { useFrame, useThree } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import { useDrag } from '@use-gesture/react';
+import { TransformControls } from "@react-three/drei";
+import { Group, Vector3 } from "three";
 
 function TestCube(props) {
-    const ref = useRef()
+    const target = useRef()
 
     const [isHovered, setIsHovered] = useState(false)
-    const [position, setPosition] = useState([-2, 3, 5]);
     const { size, viewport } = useThree();
     const aspect = size.width / viewport.width;
+    const position = new Vector3()
 
-    useFrame((state, delta) => {
-        const speed = isHovered ? 3 : 1
-        // ref.current.rotation.x += delta * speed
-        // ref.current.rotation.y += delta * speed
-        // ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2
-    })
 
-    const bind = useDrag(
-        ({ offset: [x, y] }) => {
-          const [, , ] = position;
-          setPosition([x / aspect, -y / aspect, 0]);
-          console.log(position)
-        },
-        { pointerEvents: true }
-      );
+    function setBoxPositionHandler() {
+        target.current.getWorldPosition(position)
+        console.log("Mouse up: " + JSON.stringify(position))
+    }
     return (
-        <mesh position={position} {...bind()} ref={ref}
-            onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
-            onPointerLeave={(event) => (event.stopPropagation(), setIsHovered(false))}
-        >
-            <boxGeometry args={props.size} />
-            <meshStandardMaterial color={isHovered ? "red" : props.color} />
-        </mesh>
+        <TransformControls onMouseUp={setBoxPositionHandler} translationSnap={0.5}>
+            <mesh position={props.position}
+                onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
+                onPointerLeave={(event) => (event.stopPropagation(), setIsHovered(false))}
+                ref={target}
+            >
+                <boxGeometry args={props.size} />
+                <meshStandardMaterial color={props.color} />
+            </mesh>
+        </TransformControls>
     );
 }
 
