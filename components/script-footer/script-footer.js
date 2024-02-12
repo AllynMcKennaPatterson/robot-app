56b402542ae78b19643589b5bfd58d23a2624c4b
G00392765@atu.ie <G00392765@atu.ie>
2024-02-12 09:13:08 +0000

    Added object mapping to render each element in list of script icons

--- components/script-footer/script-footer.js
+++ components/script-footer/script-footer.js
@@ -1,20 +1,54 @@
 import { useState } from "react";
 import classes from "./script-footer.module.css";
+import ScriptIcon from "../script-icons/script-icon";
+import CustomScroll from "react-custom-scroll";
 
-function ScriptFooter() {
+function ScriptFooter(props) {
   const [openFooter, setOpenFooter] = useState(false);
+  const action = [
+      {
+        "icon":"slider"
+      }, 
+      {
+        "icon": "delay",
+        "value": "700",
+      }, 
+      {
+        "icon":"endEffector"
+      },
+      {
+        "icon":"slider"
+      }, 
+      {
+        "icon": "delay",
+        "value": "500",
+      }, 
+      {
+        "icon":"endEffector"
+      }];
 
-  function handleToggle(){
+  function handleToggle(props){
     console.log("Clicked")
     setOpenFooter(!openFooter)
   }
   if(openFooter == true){
     return (
     <div className={classes.footerContainer}>
+      
+      <div className={classes.scriptContainer}>
+      <ul className={classes.list}>
+        {action.map((action) => (
+          <li className={classes.postContainer}>
+            <ScriptIcon action={action.icon} value={action.value}></ScriptIcon>
+          </li>
+          
+        ))}
+      </ul>
+      </div>
+      
       <div className={classes.triangleDown}
         onClick={handleToggle}></div>
       </div>
-    
     );
   }
   else{
@@ -24,7 +58,6 @@ function ScriptFooter() {
         <div className={classes.triangleUp}
           onClick={handleToggle}></div>
         </div>
-      
       );
   }
   
