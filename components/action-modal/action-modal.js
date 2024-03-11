3eec11fe0bc887b5c3d37cb9d384a67a37a63e78
G00392765@atu.ie <G00392765@atu.ie>
2024-03-11 11:07:10 +0000

    Added conditional rendering for coordinate modal

--- components/action-modal/action-modal.js
+++ components/action-modal/action-modal.js
@@ -11,36 +11,65 @@ function ActionModal(props) {
 
     if (!props.open) return null;
     console.log("Props action" + data)
-    return (
-        <div className={classes.container}>
-            <div className={classes.header}>
-                <div></div>
-                <h3 className={classes.title}>Action {props.index + 1}</h3>
-                <h2 className={classes.close} onClick={props.onClose}>
-                    x
-                </h2>
+    if (props.action.actionType == "slider") {
+        return (
+            <div className={classes.container}>
+                <div className={classes.header}>
+                    <div></div>
+                    <h3 className={classes.title}>Action {props.index + 1}</h3>
+                    <h2 className={classes.close} onClick={props.onClose}>
+                        x
+                    </h2>
+                </div>
+                <div className={classes.divider}></div>
+                <div className={classes.actionType}> <span style={{ fontWeight: '600' }}>Action Type:</span> {props.actionType}</div>
+                <div className={classes.angles}>
+                    <div className={classes.angle}>
+                        <p className={classes.jointLabel}>J1:</p> <p className={classes.value}>{props.action.action.val1}</p>
+                    </div>
+                    <div className={classes.angle}>
+                        <p className={classes.jointLabel}>J2:</p> <p className={classes.value}>{props.action.action.val2}</p>
+                    </div>
+                    <div className={classes.angle}>
+                        <p className={classes.jointLabel}>J3:</p> <p className={classes.value}>{props.action.action.val3}</p>
+                    </div>
+                    <div className={classes.angle}>
+                        <p className={classes.jointLabel}>J4:</p> <p className={classes.value}>{props.action.action.val4}</p>
+                    </div>
+                    <div className={classes.angle}>
+                        <p className={classes.jointLabel}>J5:</p> <p className={classes.value}>{props.action.action.val5}</p>
+                    </div>
+                </div>
             </div>
-            <div className={classes.divider}></div>
-            <div className={classes.actionType}> <span style={{fontWeight:'600'}}>Action Type:</span> {props.actionType}</div>
-            <div className={classes.angles}>
-                <div className={classes.angle}>
-                    <p className={classes.jointLabel}>J1:</p> <p className={classes.value}>{props.action.action.val1}</p>
+        );
+    }
+    else if(props.action.actionType == "endEffector"){
+        return (
+            <div className={classes.coordsContainer}>
+                <div className={classes.header}>
+                    <div></div>
+                    <h3 className={classes.title}>Action {props.index + 1}</h3>
+                    <h2 className={classes.close} onClick={props.onClose}>
+                        x
+                    </h2>
                 </div>
-                <div className={classes.angle}>
-                    <p className={classes.jointLabel}>J2:</p> <p className={classes.value}>{props.action.action.val2}</p>
-                </div>
-                <div className={classes.angle}>
-                    <p className={classes.jointLabel}>J3:</p> <p className={classes.value}>{props.action.action.val3}</p>
-                </div>
-                <div className={classes.angle}>
-                    <p className={classes.jointLabel}>J4:</p> <p className={classes.value}>{props.action.action.val4}</p>
-                </div>
-                <div className={classes.angle}>
-                    <p className={classes.jointLabel}>J5:</p> <p className={classes.value}>{props.action.action.val5}</p>
+                <div className={classes.divider}></div>
+                <div className={classes.actionType}> <span style={{ fontWeight: '600', fontSize: '14px'}}>Action Type:</span> <span style={{fontSize: '14px'}}>End Effector</span></div>
+                <div className={classes.coords}>
+                    <div className={classes.angle}>
+                        <p className={classes.jointLabel}>X:</p> <p className={classes.value}>{props.action.action.val1}</p>
+                    </div>
+                    <div className={classes.angle}>
+                        <p className={classes.jointLabel}>Y:</p> <p className={classes.value}>{props.action.action.val2}</p>
+                    </div>
+                    <div className={classes.angle}>
+                        <p className={classes.jointLabel}>Z:</p> <p className={classes.value}>{props.action.action.val3}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+
 }
 
 export default ActionModal;
