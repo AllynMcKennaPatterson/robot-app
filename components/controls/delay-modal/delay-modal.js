2b3faed7c6a422eacebb5d09c2d61bb20f499a87
G00392765@atu.ie <G00392765@atu.ie>
2024-03-11 11:07:30 +0000

    Created delay modal

--- components/controls/delay-modal/delay-modal.js
+++ components/controls/delay-modal/delay-modal.js
@@ -5,11 +5,40 @@ import ModelContext from "@/pages/Store/modelContext";
 import ScriptContext from "@/pages/Store/scriptContext";
 
 function DelayModal(props) {
-  if (!props.open) return null;
+  const [delay, setDelay] = useState(0);
+  const scriptCtx = useContext(ScriptContext);
 
+  async function scriptActionHandler() {
+    console.log("Delay: " + delay)
+    if(delay >= 0){
+      await scriptCtx.addDelayData(delay);
+    }
+    else{
+      console.log("Delay cannot be negative")
+    }
+  }
+
+  if (!props.open) return null;
   return (
     <div className={classes.modalContainer}>
-        test
+      <div className={classes.header}>
+        <div></div>
+        <h3 className={classes.title}>Delay</h3>
+        <h2 className={classes.close} onClick={props.onClose}>
+          x
+        </h2>
+      </div>
+      <div className={classes.divider}></div>
+      <div className={classes.delayInputContainer}>
+        <div style={{marginLeft: '8px', marginRight: '8px'}}></div>
+          <input className={classes.delayInput} type="number" name="test" min={0} oninput="validity.valid||(value='');" onChange={e => setDelay(e.target.value)}/>
+          <p style={{marginLeft: '8px', marginRight: '8px'}}>ms</p>
+      </div>
+      <div className={classes.scriptButtonContainer} onClick={scriptActionHandler}>
+        <div className={classes.scriptButtonContent}>
+          <h3 className={classes.scriptButtonText}>Add To Script</h3>
+        </div>
+      </div>
     </div>
   );
 }
