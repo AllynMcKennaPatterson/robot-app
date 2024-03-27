d09cf87c1732b4bc17632d3df8b3f74d4e2b71b3
Allyn McKenna Patterson <allyncodes@gmail.com>
2024-03-27 23:53:59 +0000

    Fixed transform controls position relative to block

--- components/react-three-fiber/test-cube/test-cube.js
+++ components/react-three-fiber/test-cube/test-cube.js
@@ -17,19 +17,23 @@ function TestCube(props) {
 
     function setBoxPositionHandler() {
         target.current.getWorldPosition(position)
+        let realPosition = {x: position.x, y: (position.y+3), z: (position.z+5)}
         console.log("Mouse up: " + JSON.stringify(position))
-        modelCtx.setTargetBlockPosition(position)
+        console.log("Target block: " + JSON.stringify(modelCtx.targetBlockPos))
+        modelCtx.setTargetBlockPosition(realPosition)
     }
     return (
-        <TransformControls onMouseUp={setBoxPositionHandler} translationSnap={0.5}>
-            <mesh position={props.position}
+        <TransformControls onMouseUp={setBoxPositionHandler} translationSnap={0.5} position={[0, 4, 10]}>
+            <group position={[0, -3, -5]}>
+               <mesh position={props.position}
                 // onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
                 // onPointerLeave={(event) => (event.stopPropagation(), setIsHovered(false))}
                 ref={target}
             >
                 <boxGeometry args={props.size} />
                 <meshStandardMaterial color={props.color} />
-            </mesh>
+            </mesh> 
+            </group>
         </TransformControls>
     );
 }
